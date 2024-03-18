bcc5e1a454346d84f9096ad7c785b4130da8c3d6
Alkan Aziz <130231095+alkanaziz@users.noreply.github.com>
2024-03-18 21:17:23 +0100

    Alkan: Updated image URLs in DressStyle component

--- src/components/DressStyle.jsx
+++ src/components/DressStyle.jsx
@@ -13,16 +13,16 @@ function DressStyle() {
           BROWSE BY DRESS STYLE
         </h2>
         <div className="grid grid-cols-1 gap-8 py-7 *:relative *:h-48 *:rounded-3xl *:bg-cover *:bg-top *:font-satoshi_bold *:text-2xl md:grid-cols-5 md:*:h-72 lg:grid-cols-7 lg:py-16 lg:*:text-4xl">
-          <div className="style-casual bg-[url('images/dressStyle/casual.png')] md:col-span-2 lg:col-span-3">
+          <div className="style-casual bg-[url('/images/dressStyle/casual.png')] md:col-span-2 lg:col-span-3">
             <h3 className="absolute left-6 top-9">Casual</h3>
           </div>
-          <div className="style-formal bg-[url('images/dressStyle/formal.png')] md:col-span-3 lg:col-span-4">
+          <div className="style-formal bg-[url('/images/dressStyle/formal.png')] md:col-span-3 lg:col-span-4">
             <h3 className="absolute left-6 top-9">Formal</h3>
           </div>
-          <div className="style-party bg-[url('images/dressStyle/party.png')] md:col-span-3 lg:col-span-4">
+          <div className="style-party bg-[url('/images/dressStyle/party.png')] md:col-span-3 lg:col-span-4">
             <h3 className="absolute left-6 top-9">Party</h3>
           </div>
-          <div className="style-gym bg-[url('images/dressStyle/gym.png')] md:col-span-2 lg:col-span-3">
+          <div className="style-gym bg-[url('/images/dressStyle/gym.png')] md:col-span-2 lg:col-span-3">
             <h3 className="absolute left-6 top-9">Gym</h3>
           </div>
         </div>

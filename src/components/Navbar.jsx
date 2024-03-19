c7f1a4ae719c8224d6820abb55c80dcd11c71247
PhilV1 <werpub.phil@gmx.de>
2024-03-19 17:01:38 +0100

    Add social media icons to the Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,10 @@ import { IoSearch } from 'react-icons/io5'
 import { LuShoppingCart } from 'react-icons/lu'
 import { CgProfile } from 'react-icons/cg'
 import { FaAngleDown } from 'react-icons/fa6'
+import { FaTwitter } from 'react-icons/fa'
+import { FaFacebook } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
 
 function Navbar() {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
@@ -25,10 +29,10 @@ function Navbar() {
           </div>
           {/* Dropdown */}
           <div
-            className={`absolute inset-y-16 left-0 z-10 flex h-full w-screen justify-center bg-white  font-satoshi_regular transition-all duration-500 ease-in-out lg:hidden ${menuIsOpen ? ' h-screen' : '  h-0 opacity-0'}`}
+            className={`absolute inset-y-16 left-0 z-10 flex w-screen   justify-center bg-white  font-satoshi_regular transition-all duration-500 ease-in-out lg:hidden ${menuIsOpen ? ' h-dvh  overflow-hidden' : '  h-0 '}`}
           >
             <ul
-              className={`transition-all duration-500 ease-in-out ${menuIsOpen ? 'opacity-100' : 'opacity-0'}`}
+              className={`mt-10 flex flex-col items-center font-satoshi_regular transition-all duration-500 ease-in-out ${menuIsOpen ? 'opacity-100' : 'opacity-0'}`}
             >
               <NavLink to="/" onClick={() => setMenuIsOpen(!menuIsOpen)}>
                 <li>Home</li>
@@ -36,6 +40,36 @@ function Navbar() {
               <NavLink to="/cart" onClick={() => setMenuIsOpen(!menuIsOpen)}>
                 <li>Cart</li>
               </NavLink>
+              <div className="flex space-x-3 pt-10">
+                <a
+                  href="https://twitter.com"
+                  target="_blank"
+                  className="rounded-full border-[1px] bg-white px-2 py-2"
+                >
+                  <FaTwitter />
+                </a>
+                <a
+                  href="https://facebook.com"
+                  target="_blank"
+                  className="rounded-full border-[1px] bg-white px-2 py-2"
+                >
+                  <FaFacebook />
+                </a>
+                <a
+                  href="https://instagram.com"
+                  target="_blank"
+                  className="rounded-full border-[1px] bg-white px-2 py-2"
+                >
+                  <FaInstagram />
+                </a>
+                <a
+                  href="https://github.com"
+                  target="_blank"
+                  className="rounded-full border-[1px] bg-white px-2 py-2"
+                >
+                  <FaGithub />
+                </a>
+              </div>
             </ul>
           </div>
 

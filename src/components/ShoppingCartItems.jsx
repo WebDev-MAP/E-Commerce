363c0cf1d1f4263846311c1e5af2c8516a3c8b10
Alkan Aziz <130231095+alkanaziz@users.noreply.github.com>
2024-04-16 11:44:11 +0200

    Add promo code functionality to ShopContext.jsx and Cart (#54)

--- src/components/ShoppingCartItems.jsx
+++ src/components/ShoppingCartItems.jsx
@@ -2,6 +2,8 @@ import { PiTrashFill } from 'react-icons/pi'
 import { formatCurrency } from '../utilities/formatCurrency'
 import { useShopContext } from '../context/ShopContext'
 import { products } from '../data/products'
+import { Link } from 'react-router-dom'
+import Button from './Button'
 
 function ShoppingCartItems() {
   const {
@@ -15,83 +17,94 @@ function ShoppingCartItems() {
     <>
       <section className="cart-products rounded-[20px] border md:w-7/12">
         <ul className="divide-y px-3 md:px-6">
-          { cartItems.length > 0 ? (
-          cartItems.map((cartItem) =>
-            products.map((product) => {
-              if (product.id === cartItem.id) {
-                return (
-                  <li key={cartItem.id.toString()}>
-                    <section className="my-3 flex w-full gap-3 md:my-6">
-                      <div>
-                        <img
-                          className="w-28 md:w-32"
-                          src={product.mainImage}
-                          alt={product.title}
-                        />
-                      </div>
-                      <div className="flex w-full flex-col justify-between">
-                        <div className="flex justify-between">
-                          <div>
-                            <h3 className="font-satoshi_bold text-sm md:text-xl">
-                              {product.title}
-                            </h3>
-                            <p className="font-satoshi_regular text-[12px] md:text-sm">
-                              Size:{' '}
-                              <span className="text-black text-opacity-60">
-                                {cartItem.size}
-                              </span>
-                            </p>
-                            <p className="font-satoshi_regular text-[12px] md:text-sm">
-                              Color:{' '}
-                              <span className="text-black text-opacity-60">
-                                {cartItem.color}
-                              </span>
-                            </p>
+          {cartItems.length > 0 ? (
+            cartItems.map((cartItem) =>
+              products.map((product) => {
+                if (product.id === cartItem.id) {
+                  return (
+                    <li key={cartItem.id.toString()}>
+                      <section className="my-3 flex w-full gap-3 md:my-6">
+                        <div>
+                          <img
+                            className="w-28 md:w-32"
+                            src={product.mainImage}
+                            alt={product.title}
+                          />
+                        </div>
+                        <div className="flex w-full flex-col justify-between">
+                          <div className="flex justify-between">
+                            <div>
+                              <h3 className="font-satoshi_bold text-sm md:text-xl">
+                                {product.title}
+                              </h3>
+                              <p className="font-satoshi_regular text-[12px] md:text-sm">
+                                Size:{' '}
+                                <span className="text-black text-opacity-60">
+                                  {cartItem.size}
+                                </span>
+                              </p>
+                              <p className="font-satoshi_regular text-[12px] md:text-sm">
+                                Color:{' '}
+                                <span className="text-black text-opacity-60">
+                                  {cartItem.color}
+                                </span>
+                              </p>
+                            </div>
+                            <div>
+                              <button>
+                                <PiTrashFill
+                                  onClick={() => removeCartItem(product.id)}
+                                  className="h-5 w-5 text-[#FF3333]"
+                                />
+                              </button>
+                            </div>
                           </div>
-                          <div>
-                            <button>
-                              <PiTrashFill
-                                onClick={() => removeCartItem(product.id)}
-                                className="h-5 w-5 text-[#FF3333]"
-                              />
-                            </button>
+                          <div className="flex items-center justify-between">
+                            <p className="font-satoshi_bold text-xl md:text-2xl">
+                              {formatCurrency(
+                                product.price * cartItem.quantity
+                              )}
+                            </p>
+                            <div className="flex rounded-full bg-[#F0F0F0]">
+                              <button
+                                onClick={() => decreaseCartQuantity(product.id)}
+                                className="rounded-l-full py-1 pl-3 pr-4 text-[1rem] outline-1 hover:outline"
+                              >
+                                -
+                              </button>
+                              <span
+                                className="flex w-12 items-center justify-center bg-[#F0F0F0]"
+                                id="quantity"
+                              >
+                                {cartItem.quantity}
+                              </span>
+                              <button
+                                onClick={() => increaseCartQuantity(product.id)}
+                                className="rounded-r-full py-1 pl-4 pr-3 text-[1rem] outline-1 hover:outline"
+                              >
+                                +
+                              </button>
+                            </div>
                           </div>
                         </div>
-                        <div className="flex items-center justify-between">
-                          <p className="font-satoshi_bold text-xl md:text-2xl">
-                            {formatCurrency(product.price * cartItem.quantity)}
-                          </p>
-                          <div className="flex rounded-full bg-[#F0F0F0]">
-                            <button
-                              onClick={() => decreaseCartQuantity(product.id)}
-                              className="rounded-l-full py-1 pl-3 pr-4 text-[1rem] outline-1 hover:outline"
-                            >
-                              -
-                            </button>
-                            <span
-                              className="flex w-12 items-center justify-center bg-[#F0F0F0]"
-                              id="quantity"
-                            >
-                              {cartItem.quantity}
-                            </span>
-                            <button
-                              onClick={() => increaseCartQuantity(product.id)}
-                              className="rounded-r-full py-1 pl-4 pr-3 text-[1rem] outline-1 hover:outline"
-                            >
-                              +
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </section>
-                  </li>
-                )
-              } else {
-                return null
-              }
-            })
-          )) : 
-          (<div className="flex justify-center items-center m-3">Your cart is empty</div>)}
+                      </section>
+                    </li>
+                  )
+                } else {
+                  return null
+                }
+              })
+            )
+          ) : (
+            <div className="m-3 flex flex-col items-center justify-center">
+              <p>Your cart is empty</p>
+              <Link to="/category">
+                <Button className="w-full md:my-8 md:w-fit lg:mb-12" primary>
+                  Shop Now
+                </Button>
+              </Link>
+            </div>
+          )}
         </ul>
       </section>
     </>

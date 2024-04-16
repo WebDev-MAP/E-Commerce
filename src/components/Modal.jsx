f3d98e3d8536e60ca4cdf5dcb8519a61413704bf
PhilV1 <werpub.phil@gmx.de>
2024-04-16 15:01:31 +0200

    Adjust position of actionbar in Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,10 +16,10 @@ function Modal({ onClose, children, actionbar }) {
         onClick={onClose}
         className="fixed inset-0 z-40 bg-gray-300 opacity-80"
       ></div>
-      <div className="fixed  inset-20 z-50 rounded-3xl bg-white  p-10 md:inset-40 md:top-20">
+      <div className="fixed   left-5 right-5 top-20 z-50 rounded-3xl bg-white  p-10 md:left-48 md:right-48 md:top-20 ">
         <div className="flex  flex-col">
           {children}
-          <div className="flex justify-center pt-28">{actionbar}</div>
+          <div className="flex justify-center pt-10 md:pt-28">{actionbar}</div>
         </div>
       </div>
     </div>,
